8a10f9500c81e46b17fb9662d095514c8543be97
hoangphuc348589-hub <hoangphuc348589@gmail.com>
2026-06-24 19:23:55 +0700

    Update Teacher.cpp
    đã thêm các chức năng còn lại

--- Teacher.cpp
+++ Teacher.cpp
@@ -4,4 +4,37 @@ void Teacher::addClassId(string classId) {
     this->classIds.push_back(classId);
 }
 
-void Teacher::display() {}
+void Teacher::display() 
+// phúc : xem danh sách lớp, danh sách sinh viên từng lớp, xem sửa điểm sv//
+{
+    cout << "Teacher ID: " << id << endl;
+    cout << "Teacher Name: " << name << endl;
+
+    cout << "Classes:" << endl;
+    for (string c : classIds) {
+        cout << "- " << c << endl;
+    }
+}
+
+void Teacher::editGrade(Student &st) {
+    cout << "\nStudent: " << st.getName() << endl;
+
+    cout << "Current process grade: "
+         << st.getProcessGrade() << endl;
+
+    cout << "Current final grade: "
+         << st.getFinalGrade() << endl;
+
+    double process, finalGrade;
+
+    cout << "New process grade: ";
+    cin >> process;
+
+    cout << "New final grade: ";
+    cin >> finalGrade;
+
+    st.setProcessGrade(process);
+    st.setFinalGrade(finalGrade);
+
+    cout << "Update successful!" << endl;
+}
